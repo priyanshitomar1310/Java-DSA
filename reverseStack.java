47895a2122f769366e6abd3103b7e29d98fdcd97
priyanshitomar1310 <priyanshitomar1506@gmail.com>
2025-04-17 06:49:52 +0530

    Add files via upload

--- reverseStack.java
+++ reverseStack.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class reverseStack{
-    class stack2{
-        public void pushBottom(Stack<Integer> s,int data)
+    static class stack2{
+        public static void pushBottom(Stack<Integer> s,int data)
         {
             if(s.isEmpty())
             {
@@ -13,16 +13,33 @@ class reverseStack{
             s.push(top);
         }
     }
+    public static void reverse(Stack<Integer> s)
+    {
+        if(s.isEmpty())
+        {
+            return;
+        }
+        int top=s.pop();
+        reverse(s);
+        stack2.pushBottom(s,top);
+
+    }
+    public static void printStack(Stack<Integer> s)
+    {
+         while(!s.isEmpty())
+         {
+            System.out.print(s.peek()+" ");
+            s.pop();
+         }
+    }
     public static void main(String args[])
     {
-           Stack<Integer> st=new Stack<>();
-           st.push(1);
-           st.push(2);
-           st.push(3);
-           while(!st.isEmpty())
-           {
-               System.out.print(st.peek()+" ");
-               st.pop();
-           }
+           Stack<Integer> s=new Stack<>();
+           s.push(1);
+           s.push(2);
+           s.push(3);
+           reverse(s);
+           printStack(s);
+
     }
 }
